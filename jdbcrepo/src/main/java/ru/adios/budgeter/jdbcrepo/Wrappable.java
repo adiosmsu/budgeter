cad41a893a07b081dfa622d12901a025ff49d05d
adios <adiosmsu@gmail.com>
2015-11-01 03:29:20 +0300

    jdbcrepo: Wrappable final clean-up.

--- jdbcrepo/src/main/java/ru/adios/budgeter/jdbcrepo/Wrappable.java
+++ jdbcrepo/src/main/java/ru/adios/budgeter/jdbcrepo/Wrappable.java
@@ -25,40 +25,28 @@ interface Wrappable extends AutoCloseable {
         }
     }
 
-    static <ReturnType, Param> ReturnType wrapWithClose(AutoCloseable s, Function<Param, ReturnType> f, Param p) {
+    default <ReturnType, Param> ReturnType wrap(Function<Param, ReturnType> f, Param p) {
         try {
             return f.apply(p);
         } finally {
-            closeSilently(s);
+            closeSilently(this);
         }
-    }
-
-    static <Param> void wrapWithClose(AutoCloseable s, Consumer<Param> c, Param p) {
-        try {
-            c.accept(p);
-        } finally {
-            closeSilently(s);
-        }
-    }
-
-    static <ReturnType> ReturnType wrapWithClose(AutoCloseable s, Supplier<ReturnType> f) {
-        try {
-            return f.get();
-        } finally {
-            closeSilently(s);
-        }
-    }
-
-    default <ReturnType, Param> ReturnType wrap(Function<Param, ReturnType> f, Param p) {
-        return wrapWithClose(this, f, p);
     }
 
     default <Param> void wrap(Param p, Consumer<Param> c) {
-        wrapWithClose(this, c, p);
+        try {
+            c.accept(p);
+        } finally {
+            closeSilently(this);
+        }
     }
 
     default <ReturnType> ReturnType wrap(Supplier<ReturnType> f) {
-        return wrapWithClose(this, f);
+        try {
+            return f.get();
+        } finally {
+            closeSilently(this);
+        }
     }
 
 }
